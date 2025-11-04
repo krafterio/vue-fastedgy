27ba5dd436257316800066b8712bcdd4100997e6
François Pluchino <francois@krafter.io>
2025-11-04 12:41:02 +0100

    feat(core): expose setToken methods in auth store

--- types/stores/auth.d.ts
+++ types/stores/auth.d.ts
@@ -1,18 +1,10 @@
 export const useAuthStore: import("pinia").StoreDefinition<"auth", Pick<{
     user: import("vue").Ref<any, any>;
     token: import("vue").Ref<string, string>;
-    refreshToken: import("vue").Ref<string, string>;
     loading: import("vue").Ref<boolean, boolean>;
     isAuthenticated: import("vue").ComputedRef<boolean>;
     canRefreshToken: import("vue").ComputedRef<boolean>;
     isTokenExpired: import("vue").ComputedRef<boolean>;
-    login: (credentials: any) => Promise<{
-        success: boolean;
-        message?: undefined;
-    } | {
-        success: boolean;
-        message: any;
-    }>;
     register: (userData: any, invitationToken?: any) => Promise<{
         success: boolean;
         message?: undefined;
@@ -20,25 +12,27 @@ export const useAuthStore: import("pinia").StoreDefinition<"auth", Pick<{
         success: boolean;
         message: any;
     }>;
+    login: (credentials: any) => Promise<{
+        success: boolean;
+        message?: undefined;
+    } | {
+        success: boolean;
+        message: any;
+    }>;
     logout: () => Promise<void>;
+    setToken: (newToken: any) => void;
+    setTokens: (accessToken: any, newRefreshToken: any) => void;
+    refreshToken: import("vue").Ref<string, string>;
     refreshAccessToken: () => Promise<boolean>;
     checkUser: () => Promise<any>;
     refreshUser: () => Promise<any>;
-}, "user" | "token" | "refreshToken" | "loading">, Pick<{
+}, "user" | "token" | "loading" | "refreshToken">, Pick<{
     user: import("vue").Ref<any, any>;
     token: import("vue").Ref<string, string>;
-    refreshToken: import("vue").Ref<string, string>;
     loading: import("vue").Ref<boolean, boolean>;
     isAuthenticated: import("vue").ComputedRef<boolean>;
     canRefreshToken: import("vue").ComputedRef<boolean>;
     isTokenExpired: import("vue").ComputedRef<boolean>;
-    login: (credentials: any) => Promise<{
-        success: boolean;
-        message?: undefined;
-    } | {
-        success: boolean;
-        message: any;
-    }>;
     register: (userData: any, invitationToken?: any) => Promise<{
         success: boolean;
         message?: undefined;
@@ -46,25 +40,27 @@ export const useAuthStore: import("pinia").StoreDefinition<"auth", Pick<{
         success: boolean;
         message: any;
     }>;
+    login: (credentials: any) => Promise<{
+        success: boolean;
+        message?: undefined;
+    } | {
+        success: boolean;
+        message: any;
+    }>;
     logout: () => Promise<void>;
+    setToken: (newToken: any) => void;
+    setTokens: (accessToken: any, newRefreshToken: any) => void;
+    refreshToken: import("vue").Ref<string, string>;
     refreshAccessToken: () => Promise<boolean>;
     checkUser: () => Promise<any>;
     refreshUser: () => Promise<any>;
 }, "isAuthenticated" | "canRefreshToken" | "isTokenExpired">, Pick<{
     user: import("vue").Ref<any, any>;
     token: import("vue").Ref<string, string>;
-    refreshToken: import("vue").Ref<string, string>;
     loading: import("vue").Ref<boolean, boolean>;
     isAuthenticated: import("vue").ComputedRef<boolean>;
     canRefreshToken: import("vue").ComputedRef<boolean>;
     isTokenExpired: import("vue").ComputedRef<boolean>;
-    login: (credentials: any) => Promise<{
-        success: boolean;
-        message?: undefined;
-    } | {
-        success: boolean;
-        message: any;
-    }>;
     register: (userData: any, invitationToken?: any) => Promise<{
         success: boolean;
         message?: undefined;
@@ -72,9 +68,19 @@ export const useAuthStore: import("pinia").StoreDefinition<"auth", Pick<{
         success: boolean;
         message: any;
     }>;
+    login: (credentials: any) => Promise<{
+        success: boolean;
+        message?: undefined;
+    } | {
+        success: boolean;
+        message: any;
+    }>;
     logout: () => Promise<void>;
+    setToken: (newToken: any) => void;
+    setTokens: (accessToken: any, newRefreshToken: any) => void;
+    refreshToken: import("vue").Ref<string, string>;
     refreshAccessToken: () => Promise<boolean>;
     checkUser: () => Promise<any>;
     refreshUser: () => Promise<any>;
-}, "login" | "register" | "logout" | "refreshAccessToken" | "checkUser" | "refreshUser">>;
+}, "register" | "login" | "logout" | "setToken" | "setTokens" | "refreshAccessToken" | "checkUser" | "refreshUser">>;
 //# sourceMappingURL=auth.d.ts.map
